5acba35259f237f7740c40669ebf7480d573dfff
wesleyvanneck <wesleyvanneck105@outlook.com>
2018-10-07 16:41:05 +0200

    Update Normal.php

--- src/pocketmine/level/generator/normal/Normal.php
+++ src/pocketmine/level/generator/normal/Normal.php
@@ -151,8 +151,8 @@ class Normal extends Generator{
 
 	public function generateChunk(int $chunkX, int $chunkZ) : void{
 $this->random->setSeed(0xdeadbeef ^ ($chunkX << 8) ^ $chunkZ ^ $this->level->getSeed());
-
-		$noise = Generator::getFastNoise3D($this->noiseBase, 16, 256, 16, 4, 8, 4, $chunkX * 16, 0, $chunkZ * 16);
+		
+		//$noise = Generator::getFastNoise3D($this->noiseBase, 16, 256, 16, 4, 8, 4, $chunkX * 16, 0, $chunkZ * 16);
 
 		$chunk = $this->level->getChunk($chunkX, $chunkZ);
 
