f0a157b72392b72bd7e876ba33ecf2106c5bf827
wesleyvanneck <wesleyvanneck105@outlook.com>
2018-10-15 09:03:06 +0200

    fix

--- src/pocketmine/math/VectorMath.php
+++ src/pocketmine/math/VectorMath.php
@@ -30,4 +30,15 @@ abstract class VectorMath{
 		return new Vector2(cos($azimuth), sin($azimuth));
 	}
 
+	/**
+	 * @param $azimuth
+	 * @param $inclination
+	 *
+	 * @return Vector3
+	 */
+	public static function getDirection3D($azimuth, $inclination) : Vector3{
+		$yFact = cos($inclination);
+		return new Vector3($yFact * cos($azimuth), sin($inclination), $yFact * sin($azimuth));
+	}
+
 }
