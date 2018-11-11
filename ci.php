361e3c0c37b69624696e228daa89040f289c3260
YasunaMC <wesleyvanneck105@outlook.com>
2018-11-11 19:24:24 +0100

    Update ci.php

--- ci.php
+++ ci.php
@@ -1,34 +1,26 @@
 <?php
+// import from clearsky/ci-test
 $time = time();
 $port = rand(1000,60000);
 while(system("lsof -i:".$port) != null){
 	$port = rand(1000,60000);
 }
-echo "php: ".PHP_VERSION.PHP_EOL;
-echo "port: ".$port.PHP_EOL;
+echo "port is ".$port.PHP_EOL;
 system("echo \"server-port=".$port."\" > server.properties");
-system("echo \"DevTools=on\" > bluelight.properties");
-$server = proc_open(PHP_BINARY . " src/pocketmine/PocketMine.php --no-wizard --disable-readline --settings.enable-dev-builds=1", [
+$server = proc_open(PHP_BINARY . " src/pocketmine/PocketMine.php --no-wizard --disable-readline --settings.enable-dev-builds", [
 	0 => ["pipe", "r"],
 	1 => ["pipe", "w"],
 	2 => ["pipe", "w"]
 ], $pipes);
-fwrite($pipes[0],
-	"version".PHP_EOL.
-	"makeserver".PHP_EOL.
-	"stop".PHP_EOL.PHP_EOL
-);
+fwrite($pipes[0], "version\nmakeserver\nstop\n\n");
 while(!feof($pipes[1]) and time()-$time<60*3){
 	echo fgets($pipes[1]);
 }
 fclose($pipes[0]);
 fclose($pipes[1]);
 fclose($pipes[2]);
-echo "\nReturn value: ". proc_close($server) ."\n";
-if(count(glob("plugins/Altay/Apollo_v4.0.0.phar")) === 0){
-echo "Server phar created!\n";
-   exit(0);
-  }else{
-echo "No server phar created!\n";
-   exit(1);
+echo "\n\nReturn value: ". proc_close($server) ."\n";
+if(count(glob("plugins/Altay/*.phar")) === 0){
+	echo "Server phar created!\n";
+	exit(0);
 }
